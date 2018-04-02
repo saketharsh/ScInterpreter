275ed29dd7119911b977eed38579ac64e6756c1a
saketharsh <sharsh2010@gmail.com>
2018-04-02 23:24:17 +0530

    Basic error handling Completed

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cs653project
 Course Project for CS 653 (Functional Progarmming)
+
+## Dependencies 
+Install cabal for package management in Haskell. 
+Install packages such as : Parsec  
+
+## Using the Lisp Interpreter 
+For now, run the command ` runhaskell recursiveparser.hs "the command that you want to parse" `
+
--- recursiveparser.hs
+++ recursiveparser.hs
@@ -1,15 +1,25 @@
-module Main where 
+module Main where
+
+import Data.Char 
+import Data.Array
 import Control.Monad
-import Control.Monad.Error
+import Control.Monad.Error -- for error handling in Lisp 
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces) -- defined our own spaces 
 
 main :: IO ()  -- left to add eval function over the readExpr, in order to get the output
 main = do 
 	args <- getArgs
-	putStrLn ( (show . eval . readExpr) (args !! 0))
+	evaled <- return $ liftM show $ readExpr (args !! 0 ) >>= eval
+	putStrLn $ extractValue $ trapError evaled
 
  ------------------------------------Parsing the Lisp Style Syntax ---------------------------
+
+readExpr :: String -> ThrowsError LispVal  
+readExpr input = case parse parseExpr "lisp" input of 
+	Left err ->  throwError $ Parser err
+	Right val ->  return val
+
 
 symbol :: Parser Char   -- parser that recognises the symbol
 symbol = oneOf "!$%|*+-/:<=?>@^_~#"
@@ -17,10 +27,6 @@ symbol = oneOf "!$%|*+-/:<=?>@^_~#"
 spaces :: Parser () -- additonal features to recognize spaces
 spaces = skipMany1 space
 
-readExpr :: String -> LispVal  -- change the data type to String -> LispVal , coz we will evaluate the output from thid function
-readExpr input = case parse parseExpr "lisp" input of 
-	Left err -> String $ "No Match: " ++ show err
-	Right val ->  val
 
 data LispVal = Atom String  --data type support for Scheme 
 	| List [LispVal]
@@ -29,13 +35,6 @@ data LispVal = Atom String  --data type support for Scheme
 	| String String
 	| Bool Bool
 
-data LispError = NumArgs Integer [LispVal]
-	| TypeMismatch String LispVal
-	| Parser ParseError
-	| BadSpeciaForm String LispVal
-	| NotFunction String String
-	| Unboundvar String String
-	| Default String
 
 parseString :: Parser LispVal
 parseString = do 
@@ -87,24 +86,6 @@ parseExpr = parseAtom
 
 
 instance Show LispVal where show = showVal -- helps us to print LispVal type values
-instance Show LispError where show = showError
-
-instance Error LispError where 
-	noMsg = Default "An error has occured"
-	strMsg = Default
-
-unwordsList :: [LispVal] -> String -- helper function to print lists in the interpreter 
-unwordsList = unwords . map  showVal
-
-type ThrowsError = Either LispError
-
-showError :: LispError -> String
-showError (UnboundVar message varname) = message ++ ": " ++ varname
-showError (BadSpecialForm messafe form) = message ++ ": " ++ show form
-showError (NotFunction message func) = message ++ ": " ++ show func
-showError (NumArgs expected found ) = "Expected " ++ show expected ++ "args :: found values " ++ unwordsList found
-showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++ ", found : " ++ show found 
-showError (Parser parseError ) = "Parse error at " ++ show parseErr
 
 
 showVal :: LispVal -> String   -- the basic printing function of our interpreter
@@ -116,38 +97,72 @@ showVal (Bool False ) = "#f"
 showVal (List contents ) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++  showVal tail ++ ")"
 
+unwordsList :: [LispVal] -> String -- helper function to print lists in the interpreter 
+unwordsList = unwords . map  showVal
+
+eval :: LispVal -> ThrowsError LispVal
+eval val@(String _ ) =return  val
+eval val@(Number _ ) = return val
+eval val@(Bool _) = return val
+eval ( List [Atom "quote" , val] ) = return val
+eval (List (Atom func : args)) = mapM eval args >>= apply func
+eval badForm = throwError $ BadSpecialForm "Unrecognized special Form"  badForm
+
+apply :: String -> [LispVal] -> ThrowsError LispVal
+apply func args = maybe ( throwError $ NotFunction " Unrecognized primitive function args" func ) ($ args) $ (lookup func primitives)
 
 
-eval :: LispVal -> LispVal
-eval val@(String _ ) = val
-eval val@(Number _ ) = val
-eval val@(Bool _) = val
-eval ( List [Atom "quote" , val] ) = val
-eval (List (Atom func : args)) = apply func $ map eval args
-
-apply :: String -> [LispVal] -> LispVal
-apply func args = maybe ( Bool False) ($ args) $ lookup func primitives
-
-
-primitives :: [(String, [LispVal]-> LispVal)] -- primitive operations that we wish to support1
+primitives :: [(String, [LispVal]-> ThrowsError LispVal)] -- primitive operations that we wish to support1
 primitives =   [( "+" , numericBinop (+) ),
-				( "-" , numericBinop (−) ),  -- - and * are crearting some problem, comment them out and evertything is fine
-				( "*" , numericBinop (∗) ),
+				--( "-" , numericBinop (−) ),  -- - and * are crearting some problem, comment them out and evertything is fine
+				--( "*" , numericBinop (∗) ),
 				( "/" , numericBinop div ),
 				( "mod" , numericBinop mod),
 				( "quotient" , numericBinop quot),
 				( "remainder" , numericBinop rem)]
 
 
-numericBinop :: (Integer -> Integer -> Integer) -> [LispVal]-> LispVal
-numericBinop op params = Number $ foldl1 op $ map unpackNum params
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal]-> ThrowsError LispVal
+numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
+numericBinop op params = mapM unpackNum params >>= return . Number . foldl1  op
 
-unpackNum :: LispVal -> Integer
-unpackNum (Number n ) = n 
+unpackNum :: LispVal -> ThrowsError Integer
+unpackNum (Number n ) = return n 
 unpackNum (String n) = let parsed = reads n in 
 							if null parsed
-								then 0
-								else fst $ parsed !! 0
+								then throwError $ TypeMismatch "number" $ String n 
+								else return $ fst $ parsed !! 0
 unpackNum (List [n]) = unpackNum n
-unpackNum _ = 0
+unpackNum notNum  = throwError $ TypeMismatch "number" notNum
 
+--------------------------------------Error Handling for Scheme -----------------------------------------
+
+instance Show LispError where show = showError
+
+instance Error LispError where 
+	noMsg = Default "An error has occured"
+	strMsg = Default
+
+type ThrowsError = Either LispError   -- to return either the error, or the Lisp data type that we declared earlier
+
+trapError action = catchError action (return . show )
+
+extractValue :: ThrowsError a -> a 
+extractValue (Right val ) = val
+
+data LispError = NumArgs Integer [LispVal]  -- more support can be added as time and knowledge permits 
+	| TypeMismatch String LispVal
+	| Parser ParseError
+	| BadSpecialForm String LispVal
+	| NotFunction String String
+	| UnboundVar String String
+	| Default String
+
+
+showError :: LispError -> String
+showError (UnboundVar message varname) = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func) = message ++ ": " ++ show func
+showError (NumArgs expected found ) = "Expected " ++ show expected ++ "args :: found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++ ", found : " ++ show found 
+showError (Parser parseError ) = "Parse error at " ++ show parseError
